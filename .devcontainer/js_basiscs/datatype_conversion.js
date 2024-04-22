acb616f22c02b6ebda746ed85c0bb0158320ae3e
Ayush shah <76156365+AyushShah79@users.noreply.github.com>
2024-04-22 11:19:30 +0000

    config commit

---
+++ .devcontainer/js_basiscs/datatype_conversion.js
@@ -0,0 +1,21 @@
+let  scour = "34a";
+
+let x = Number(scour);
+console.log(typeof x);
+console.log(x);
+
+
+//'33' => 33
+// '33a' => NAN
+// true => 1 , false => 0
+
+let islog = 1;
+let boolean  = Boolean(islog);
+console.log(islog,boolean);
+
+// 1 >true 0 false "ayush">true
+
+let stringNumbr = 33;
+let convertstring = String(stringNumbr);
+console.log(typeof convertstring);
+console.log(convertstring);
