bd21d1250f91b6719164375d19a506972b9c1aef
Ayush shah <76156365+AyushShah79@users.noreply.github.com>
2024-04-22 11:05:31 +0000

    config commit

---
+++ .devcontainer/js_basiscs/datatype.js
@@ -0,0 +1,10 @@
+let name = "ayush";//string
+let age = 18 ; //number
+let isLogedin = true ;//boolean= true/false
+let temprature = null // null , object type
+let  price ; // undefine = when we create a variable but dont give it value then it is undefine.
+            //undefine is undefine type .
+
+
+        //we use typeof to see which type of it
+            
